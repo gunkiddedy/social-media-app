90482ea9a2133a45ad9d0f9367a85b87050fe0a3
setiawan dedy <setiawandedy@DESKTOP-J7JTO67>
2024-02-20 00:16:24 +0700

    feat: update attachment section on post components

--- resources/js/Components/app/post/PostItem.vue
+++ resources/js/Components/app/post/PostItem.vue
@@ -6,8 +6,8 @@ const props = defineProps({
   item: Object,
 })
 
-const isImage = (file) => {
-  const mime = file.mime.split("/")
+const isImage = (attachment) => {
+  const mime = attachment.mime.split("/")
   return mime[0].toLowerCase() === "image"
 }
 
@@ -17,43 +17,43 @@ onMounted(() => {
 </script>
 
 <template>
-  <div class="post-item p-8 shadow-sm mb-3 bg-white">
+  <div class="post-item px-8 py-4 shadow-sm mb-3 bg-white">
     <!-- header -->
-    <a href="">
-      <div class="flex items-center space-x-2">
-        <img
-          class="rounded-full w-11 h-11 hover:ring-2 transition-all hover:ring-gray-300 hover:scale-110"
-          :src="item.user.avatar"
-          :alt="item.user.name"
-        />
-        <div>
-          <div class="flex items-center space-x-2">
+    <div class="flex items-center space-x-2">
+      <img
+        class="rounded-full w-11 h-11 hover:ring-2 transition-all hover:ring-gray-300 hover:scale-110"
+        :src="item.user.avatar"
+        :alt="item.user.name"
+      />
+      <div>
+        <div class="flex items-center space-x-2">
+          <a href="">
             <h4
               class="text-gray-400 font-semibold capitalize leading-none hover:underline"
             >
               {{ item.user.name }}
             </h4>
-            <div
-              v-if="item.group"
-              class="group-name text-gray-400 font-semibold capitalize"
+          </a>
+          <div
+            v-if="item.group"
+            class="group-name text-gray-400 font-semibold capitalize"
+          >
             >
-              >
-              <a class="hover:underline" href="#">
-                {{ item.group.name }}
-              </a>
-            </div>
+            <a class="hover:underline" href="#">
+              {{ item.group.name }}
+            </a>
           </div>
-          <small class="text-gray-400 text-xs">{{ item.created_at }}</small>
         </div>
+        <small class="text-gray-400 text-xs">{{ item.created_at }}</small>
       </div>
-    </a>
+    </div>
 
-    <!-- body -->
+    <!-- descriptions -->
     <Disclosure v-slot="{ open }">
       <div
         v-if="!open"
         class="body text-base text-gray-500 font-medium leading-snug"
-        v-html="item.body.substring(0, 300)"
+        v-html="item.body.substring(0, 300) + '...'"
       />
 
       <DisclosurePanel class="">
@@ -65,7 +65,7 @@ onMounted(() => {
 
       <div v-if="item.body.length > 300" class="flex justify-end">
         <DisclosureButton class="mb-2">
-          <span class="text-gray-400 text-sm hover:underline">
+          <span class="text-indigo-500 text-sm hover:underline">
             {{ open ? "read less" : "read more" }}
           </span>
         </DisclosureButton>
@@ -73,13 +73,45 @@ onMounted(() => {
     </Disclosure>
 
     <!-- attachments -->
-    <div class="attachments grid grid-cols-2 gap-2">
+    <div class="attachments-container grid grid-cols-2">
       <div
-        class="attachment"
+        class="attachments"
         v-for="attachment in item.attachments"
         :key="attachment.id"
       >
-        <img v-if="isImage" class="shadow-sm" :src="attachment.url" alt="" />
+        <div
+          v-if="isImage(attachment)"
+          class="img w-[335px] h-[225px] bg-rose-300"
+        >
+          <img
+            class="img-attachment object-cover w-full h-full"
+            :src="attachment.url"
+            alt=""
+          />
+        </div>
+        <div
+          v-else
+          class="document bg-gray-100 text-base text-gray-400 w-full h-full flex flex-col items-center justify-center"
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke-width="1.5"
+            stroke="currentColor"
+            class="w-10 h-10"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M19.5 14.25v-2.625a3.375 3.375 0 0 0-3.375-3.375h-1.5A1.125 1.125 0 0 1 13.5 7.125v-1.5a3.375 3.375 0 0 0-3.375-3.375H8.25m0 12.75h7.5m-7.5 3H12M10.5 2.25H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 0 0-9-9Z"
+            />
+          </svg>
+
+          <a href="" class="hover:underline">
+            {{ attachment.name }}
+          </a>
+        </div>
       </div>
     </div>
 
@@ -87,13 +119,13 @@ onMounted(() => {
     <div class="like-comments flex justify-between items-center mt-3">
       <button
         type="button"
-        class="bg-rose-500 rounded px-3 py-0.5 text-white like"
+        class="border border-gray-500 rounded px-3 py-0.5 text-gray-500 like"
       >
         like
       </button>
       <button
         type="button"
-        class="bg-rose-500 rounded px-3 py-0.5 text-white comment"
+        class="border border-gray-500 rounded px-3 py-0.5 text-gray-500 comment"
       >
         comment
       </button>
--- resources/js/Components/app/post/PostList.vue
+++ resources/js/Components/app/post/PostList.vue
@@ -21,6 +21,18 @@ const post1 = {
       url: "https://images.unsplash.com/photo-1605379399642-870262d3d051?q=80&w=1206&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
       mime: "image/png",
     },
+    {
+      id: 3,
+      name: "test2.png",
+      url: "https://images.unsplash.com/photo-1605379399642-870262d3d051?q=80&w=1206&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      mime: "image/png",
+    },
+    {
+      id: 4,
+      name: "document-test.docx",
+      url: "#",
+      mime: "application/msword",
+    },
   ],
   body: `<p>Lorem ipsum xxx, dolor sit amet consectetur adipisicing elit. Sit illum consequuntur nostrum harum commodi, fugiat recusandae iste tempora dicta eum in iure aliquam quae perferendis cum nisi. Minima, omnis voluptas!</p>
   <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sit illum consequuntur nostrum harum commodi, fugiat recusandae iste tempora dicta eum in iure aliquam quae perferendis cum nisi. Minima, omnis voluptas!</p>
@@ -38,6 +50,14 @@ const post2 = {
     id: 1,
     name: "Laravel",
   },
+  attachments: [
+    {
+      id: 1,
+      name: "test.png",
+      url: "https://plus.unsplash.com/premium_photo-1678565869434-c81195861939?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      mime: "image/png",
+    },
+  ],
   body: `<p>Lorem ipsum xxx, dolor sit amet consectetur adipisicing elit. Sit illum consequuntur nostrum harum commodi, fugiat recusandae iste tempora dicta eum in iure aliquam quae perferendis cum nisi. Minima, omnis voluptas!</p>
   <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sit illum consequuntur nostrum harum commodi, fugiat recusandae iste tempora dicta eum in iure aliquam quae perferendis cum nisi. Minima, omnis voluptas!</p>
   Lorem ipsum xxx, dolor sit amet consectetur adipisicing elit. Sit illum consequuntur nostrum harum commodi, fugiat recusandae iste tempora dicta eum in iure aliquam quae perferendis cum nisi. Minima, omnis voluptas!</p>
